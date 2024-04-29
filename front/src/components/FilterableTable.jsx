24ffd6c432b23848fd51308268800fe2cca098be
andres-requena-uneat <91939668+andres-requena-uneat@users.noreply.github.com>
2024-04-29 17:31:24 +0000

    Se ha sacado el titulo de la tabla

--- front/src/components/FilterableTable.jsx
+++ front/src/components/FilterableTable.jsx
@@ -20,53 +20,52 @@ import {
 const FilterableTable = (props) => {
   return (
     <>
-            <h2>{props.title}</h2>
-            <div
-                className="tableHeaderFilterButton"
-            >
-                {props.filterable && <Menu>
-                    <MenuTrigger disableButtonEnhancement>
-                        <Button icon={<Filter24Regular />} />
-                    </MenuTrigger>
-                    <MenuPopover>
-                        <MenuList
-                            checkedValues={{columns: props.filteredColumnLabels}}
-                            onCheckedValueChange={props.onCheckedValueChange}>
-                            {props.columns.map(column => (
-                                <MenuItemCheckbox key={column.label} name="columns" value={column.label}>
-                                    {column.label}
-                                </MenuItemCheckbox>
-                            ))}
-                        </MenuList>
-                    </MenuPopover>
-                </Menu>}
-
-                <Table size="small" aria-label="Table with small size">
-                    <TableHeader>
-                        <TableRow>
-                            {props.columns.filter(column => column.show).map(column => (
-                                <TableHeaderCell key={column.columnKey}>
-                                    <h4>{column.label}</h4>
-                                </TableHeaderCell>
-                            ))}
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {props.data.map(element => (
-                            <TableRow key={element.id}>
-                                {props.filteredColumns.map(col => {
-                                  return (
-                                    <TableCell key={col}>
-                                        {element[col] || "-"}
-                                    </TableCell>
-                                    )
-                                })}
-                            </TableRow>
+        <div
+            className="tableHeaderFilterButton"
+        >
+            {props.filterable && <Menu>
+                <MenuTrigger disableButtonEnhancement>
+                    <Button icon={<Filter24Regular />} />
+                </MenuTrigger>
+                <MenuPopover>
+                    <MenuList
+                        checkedValues={{columns: props.filteredColumnLabels}}
+                        onCheckedValueChange={props.onCheckedValueChange}>
+                        {props.columns.map(column => (
+                            <MenuItemCheckbox key={column.label} name="columns" value={column.label}>
+                                {column.label}
+                            </MenuItemCheckbox>
                         ))}
-                    </TableBody>
-                </Table>
-            </div>
-        </>
+                    </MenuList>
+                </MenuPopover>
+            </Menu>}
+
+            <Table size="small" aria-label="Table with small size">
+                <TableHeader>
+                    <TableRow>
+                        {props.columns.filter(column => column.show).map(column => (
+                            <TableHeaderCell key={column.columnKey}>
+                                <h4>{column.label}</h4>
+                            </TableHeaderCell>
+                        ))}
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {props.data.map(element => (
+                        <TableRow key={element.id}>
+                            {props.filteredColumns.map(col => {
+                                return (
+                                <TableCell key={col}>
+                                    {element[col] || "-"}
+                                </TableCell>
+                                )
+                            })}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    </>
   )
 }
 
